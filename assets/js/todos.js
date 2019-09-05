4d48ee626f9794f29dc30beb63c258e97accd9fa
Vineet Kaur Bagga <Bagga.v@husky.neu.edu>
2019-09-05 12:39:21 -0700

    deleting todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,12 @@
+//Check Off specific Todos by Clicking
 $("li").click(function(){
 	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todos
+$("span").click(function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
